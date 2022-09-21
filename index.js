82826286b2a995da291350f40119828f2a714272
sannasichottu <sannasichottu@gmail.com>
2022-09-20 17:39:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
+
+dotenv.config();
+app.use(express.json());
+
+//mongoose connected
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,  
+}).then(console.log("Connected to MongoDB")).catch(err=>console.log(err));
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+
+//server running in browser
+app.listen("5000",() => {
+    console.log("Server is running..")
+})
---
+++ models/Category.js
@@ -0,0 +1,13 @@
+const mongoose = require ("mongoose");
+
+// post - schema
+const CategorySchema = new mongoose.Schema({
+      name:{
+        type:String,
+        required : true,
+      },
+},
+{ timestamps:true }
+);
+
+module.exports = mongoose.model("Category",CategorySchema)
